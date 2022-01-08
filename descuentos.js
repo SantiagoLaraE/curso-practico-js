769780df962210265508ae44e15a8e7fd68d4c10
Santiago Lara <oscarescobar773@gmail.com>
2022-01-07 22:55:00 -0500

    Se termina proyecto de porcentajes y descuentos y también el reto de los cupones

--- descuentos.js
+++ descuentos.js
@@ -1,37 +1,82 @@
 // const precioNormal = 120;
 // const descuento = 18;
 
-function calcularPrecioConDescuento(precioNormal, descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precioNormal * porcentajePrecioConDescuento) / 100;
+function calcularPrecioConDescuento(precioNormal, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento =
+    (precioNormal * porcentajePrecioConDescuento) / 100;
 
-    return precioConDescuento;
+  return precioConDescuento;
 }
 
-function calcularDescuento(){
+function calcularDescuento() {
+  const inputPrice = document.getElementById("inputPrice");
+  const price = inputPrice.value;
+  const priceValue = parseInt(price);
 
-    const inputPrice = document.getElementById("inputPrice");
-    const price = inputPrice.value;
-    const priceValue = parseInt(price);
-    
+  const inputDiscount = document.getElementById("inputDiscount");
+  const discount = inputDiscount.value;
+  const DiscountValue = parseInt(discount);
 
-    const inputDiscount = document.getElementById("inputDiscount");
-    const discount = inputDiscount.value;
-    const DiscountValue = parseInt(discount);
+  const total = calcularPrecioConDescuento(priceValue, DiscountValue);
 
-    const total = calcularPrecioConDescuento(priceValue, DiscountValue);
+  const outcomePrice = document.getElementById("outcomePrice");
 
-    const outcomePrice = document.getElementById("outcomePrice");
-    
-
-    outcomePrice.innerHTML = `El precio con descuento es: $${total}`;
-    
-    
+  outcomePrice.innerHTML = `El precio con descuento es: <span>$${total}</span>`;
 }
 
-// console.log({
-//     precioNormal,
-//     descuento,
-//     precioConDescuento,
-//     porcentajePrecioConDescuento
-// }
+// CREAR CUPONES DE DESCUENTO
+
+var coupons = [];
+
+function coupon(name, discount) {
+  this.name = name;
+  this.discount = discount;
+}
+
+function createCoupon() {
+  const couponName = document.getElementById("couponName");
+  const couponNameValue = couponName.value;
+
+  const couponDiscount = document.getElementById("couponDiscount");
+  const couponDiscountValue = couponDiscount.value;
+
+  const newCoupon = new coupon(couponNameValue, couponDiscountValue);
+  coupons.push(newCoupon);
+  alert("cupon creado");
+}
+
+
+//APLICANDO CUPONES
+
+function applyCoupon() {
+  // Interactuando con HTML
+  const productPrice = document.getElementById("productPrice");
+  const productPriceValue = productPrice.value;
+
+  const applyCouponName = document.getElementById("applyCouponName");
+  const applyCouponNameValue = applyCouponName.value;
+
+  const outcomePriceCoupon = document.getElementById("outcomePriceCoupon");
+
+  //Buscar cúpon
+
+  const findCoupon = coupons.find(function (coupon) {
+    return coupon.name == applyCouponNameValue;
+  });
+
+  // Condiciones si el cúpon existe o no
+
+  if (findCoupon) {
+    const couponDiscount = findCoupon.discount;
+
+    const totalCouponPrice = calcularPrecioConDescuento(
+      productPriceValue,
+      couponDiscount
+    );
+
+    outcomePriceCoupon.innerHTML = `Obtuviste un descuento del: ${couponDiscount}% <br /> Total a pagar con descuento: $${totalCouponPrice}`;
+  } else {
+    outcomePriceCoupon.innerHTML = `El cúpon ${applyCouponNameValue} no es válido`;
+  }
+}
